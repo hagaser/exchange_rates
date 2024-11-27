d44aee0e3b2e25043ea36f07c60fd29419528300
hazrys <hagasers1550@gmail.com>
2024-11-27 17:35:52 +0300

    A_little_better

--- start-server.js
+++ start-server.js
@@ -4,12 +4,15 @@ const app = express();
 const PORT = 7777;
 const HOST = 'localhost';
 
+// static path
 app.use(express.static('static'))
 
+// on url === "/" throw index.html
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/templates/index.html');
 });
 
+// on server start
 app.listen(PORT, () => {
     console.log(`Server running at http://${HOST}:${PORT}`);
 });
--- static/JS/index.js
+++ static/JS/index.js
@@ -1,60 +1,55 @@
-function showTable() {
-// ===== loading block =====
+const showTable = (allData) => {
 
-document.getElementById('loading').style.display = 'none';
+  // ========= Table =========
 
-// ========= Table =========
+  const Table = document.getElementById('table');
+  const currencySelect1 = document.getElementById('currency-select1');
+  const currencySelect2 = document.getElementById('currency-select2');
 
-const Table = document.getElementById('Table');
-const OptionName1 = document.getElementById('currency_name1');
-const OptionName2 = document.getElementById('currency_name2');
+  allData.forEach(data => {   // add all rows in the table
 
-for (let i = 0; id > i; i++) {   // add all rows in the table
-  Table.innerHTML += `<tr>
-                        <td>
-                          <h4>${currencies[i].name}</h4>
-                        </td>
+    Table.innerHTML += `<tr>
+                          <td>
+                            <p>${data.name}</p>
+                          </td>
 
-                        <td class="value">
-                          <h4>${currencies[i].value}</h4>
-                        </td>
-                      </tr>`;
+                          <td class="value">
+                            <p>${data.value}</p>
+                          </td>
+                        </tr>`;
 
-  // add all options
+    // add all options
+    currencySelect1.innerHTML += `<option>${data.name}</option>`
+    currencySelect2.innerHTML += `<option>${data.name}</option>`
 
-  OptionName1.innerHTML += `<option>${currencies[i].name}</option>`
-  OptionName2.innerHTML += `<option>${currencies[i].name}</option>`
-};
+  });
+
+  // ===== loading block =====
+  document.getElementById('loading').style.display = 'none';
+
 }
 
 // ========== calculator ==========
 
-window.onload = function() {
+window.onload = () => {
 
-  document.getElementById('button').addEventListener("click", function() {
+  document.getElementById('calc-form').addEventListener("submit", (e) => {
+    e.preventDefault()
   
     // gets the quantity and names
 
     const quantity = document.getElementById('quantity').value;
-    const currency_name1 = document.getElementById('currency_name1').value;
-    const currency_name2 = document.getElementById('currency_name2').value;
+    const currencyName1 = document.getElementById('currency-select1').value;
+    const currencyName2 = document.getElementById('currency-select2').value;
 
     // gets values of the selected currencies
 
-    for (let i = 0; id > i; i++) {
-
-      if (currencies[i].name == currency_name1) {
-        value1 = currencies[i].value;
-      }
-
-      if (currencies[i].name == currency_name2) {
-        value2 = currencies[i].value;
-      };
-    };
+    const value1 = formatedData.find(item => item.name === currencyName1).value
+    const value2 = formatedData.find(item => item.name === currencyName2).value
 
     // counting
 
-    let answer = quantity * value1 / value2;
+    const answer = quantity * value1 / value2;
     document.getElementById('answer').innerHTML = `Ответ: ${answer}`;
     
   });
